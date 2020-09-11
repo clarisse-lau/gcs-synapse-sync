8592133cf33b1dc6f0ec577173330d12a5bfcd2e
Clarisse Lau <clarisse.lau@isbscience.org>
2020-09-11 10:21:54 -0700

    update function

--- README.md
+++ README.md
@@ -12,53 +12,28 @@ Google Cloud Function code to index files in GCS bucket by creating filehandles 
 ### gcloud CLI tool
 1. Enable the [Cloud Functions API](https://console.cloud.google.com/flows/enableapi?apiid=cloudfunctions&redirect=https://cloud.google.com/functions/quickstart&_ga=2.118113162.2081301619.1590113168-88580457.1590113168)
 2. Initialize the [Cloud SDK](https://cloud.google.com/sdk/docs)
-3. Clone this repository, and edit `.env.yaml` to set environment variables:
-    - `username`: Synapse account username 
-    - `apiKey`: Synapse API Key, can be found under Settings on Synapse
+3. Clone this repository, and edit `env.yaml` to set two center-specific environment variables:
     - `synapseProjectId`: Synapse ID of project, a unique identifier with the format `syn12345678`
-    - `foldersToSync`: Comma separated list of folders in bucket to be synchronized to Synapse
+    - `foldersToSync`: A comma separated list of folders in bucket to be synchronized to Synapse
+    - *`gcProjectName` variable should remain `htan-dcc`*
 4. Change directory to within the repository, and deploy two functions using the gcloud command-line tool:
 ```
-gcloud functions deploy syn_create --runtime python37 --env-vars-file .env.yaml --trigger-resource <TRIGGER_BUCKET_NAME> --trigger-event google.storage.object.finalize
+gcloud functions deploy <center_name>_create \
+--runtime python37 \
+--env-vars-file env.yaml \
+--entry-point syn_create \
+--trigger-resource <my_bucket> \
+--trigger-event google.storage.object.finalize
 ```
 
 ```
-gcloud functions deploy syn_delete --runtime python37 --env-vars-file .env.yaml --trigger-resource <TRIGGER_BUCKET_NAME> --trigger-event google.storage.object.delete
+gcloud functions deploy <center_name>_delete \
+--runtime python37 \
+--env-vars-file env.yaml \
+--entry-point syn_delete \
+--trigger-resource <my_bucket> \
+--trigger-event google.storage.object.delete
 ```
-
-
-### GCP Console
-Deploy two functions (one for each event type trigger)
-
-#### Create 'Object Create' Function
-1. From your GCP project dashboard, navigate to the `Cloud Functions` resource
-2. Click `Create function` and provide function name
-3. Under  **Trigger** 
-    - Select `Cloud Storage`  
-    - `Event Type`: 'Finalize/Create'
-    - `Bucket`: \<your-bucket\>
-4. Upload zip containing `main.py` and `requirements.txt` files
-    - `Runtime`: Python 3.7
-    - `Function to Execute`: ‘syn_create’
-    - `Stage Bucket`: \<your-bucket\>
-5. Under **Advanced Options**, set up environment variables. 
-The function source code requires four input variables: 
-    - `username`: Synapse account username 
-    - `apiKey`: Synapse API Key, can be found under Settings on Synapse
-    - `synapseProjectId`: Synapse ID of project, a unique identifier with the format `syn12345678`
-    - `foldersToSync`: Comma separated list of folders in bucket to be synchronized to Synapse
-    
-#### Create 'Object Delete' Function
-6. Click `Create function` and provide function name
-7. Under  **Trigger** 
-    - Select `Cloud Storage`  
-    - `Event Type`: 'Delete'
-    - `Bucket`: \<your-bucket\>
-8. Upload zip containing `main.py` and `requirements.txt` files
-    - `Runtime`: Python 3.7
-    - `Function to Execute`: ‘syn_delete’
-    - `Stage Bucket`: \<your-bucket\>
-9. Under **Advanced Options**, set up your four environment variables
 
 ---
 ### Sync Existing Files
--- main.py
+++ main.py
@@ -3,14 +3,10 @@ import os
 import sys
 import uuid
 import base64
+from google.cloud import secretmanager
 from urllib.parse import unquote_plus
-
 import tempfile
 import synapseclient
-
-syn = synapseclient.Synapse()
-syn.login(email=os.environ.get('username', 'username environment variable is not set.'),
-    apiKey=os.environ.get('apiKey', 'apiKey environment variable is not set.'), silent=True)
 
 def syn_create(data, context):
     """
@@ -23,20 +19,26 @@ def syn_create(data, context):
     print(key)
     inclFolders = os.environ.get('foldersToSync', 'foldersToSync environment variable is not set.')
 
+    gc_project = os.environ.get('gcProjectName', 'gcProjectName environment variable is not set.')
+    username = get_secret('synapse_service_username', gc_project)
+    apiKey = get_secret('synapse_service_apikey', gc_project)
+
+    syn = synapseclient.Synapse()
+    syn.login(email=username, apiKey=apiKey)
+
     if key.split('/')[0] in inclFolders.split(','): 
         filename = os.path.basename(key)
         bucket = data['bucket']
-        project_id = os.environ.get('synapseProjectId', 'synapseProjectId environment variable is not set.')
+        project_id = os.environ.get('synapseProjectId','synapseProjectId environment variable is not set.')
         storage_id = syn.restGET("/projectSettings/"+project_id+"/type/upload")['locations'][0]
         
-        parent = get_parent_folder(project_id, key)
+        parent = get_parent_folder(syn, project_id, key)
         contentmd5 = base64.b64decode(data['md5Hash']).hex()
         file_id = syn.findEntityId(filename, parent)
 
         if file_id != None:
-            targetmd5 = syn.get(file_id,downloadFile=False)['md5'];
+            targetmd5 = syn.get(file_id, downloadFile=False)['md5'];
 
-        # create filehandle if it does not exist in Synapse or if existing file was modified:
         if file_id == None or contentmd5 != targetmd5: 
             size = data['size']
             contentType = data['contentType']
@@ -50,7 +52,7 @@ def syn_create(data, context):
                                 'key'         : key,
                                 'storageLocationId': storage_id}
             fileHandle = syn.restPOST('/externalFileHandle/googleCloud', json.dumps(fileHandle), endpoint=syn.fileHandleEndpoint)
-            f = synapseclient.File(parentId=parent, dataFileHandleId=fileHandle['id'], name=filename, synapseStore=False)
+            f = synapseclient.File(parentId=parent, dataFileHandleId=fileHandle['id'], name=filename,synapseStore=False)
             f = syn.store(f)
 
 def syn_delete(data, context):
@@ -61,16 +63,29 @@ def syn_delete(data, context):
     """
     key = data['name']
     inclFolders = os.environ.get('foldersToSync', 'foldersToSync environment variable is not set.') 
+
+    gc_project = os.environ.get('gcProjectName', 'gcProjectName environment variable is not set.')
+    username = get_secret('synapse_service_username', gc_project)
+    apiKey = get_secret('synapse_service_apikey', gc_project)
+
+    syn = synapseclient.Synapse()
+    syn.login(email=username, apiKey=apiKey)
     
     if key.split('/')[0] in inclFolders.split(','):
         filename = os.path.basename(key)
         print("filename: " + filename)
         project_id = os.environ.get('synapseProjectId', 'Specified environment variable is not set.')
-        parent_id = get_parent_folder(project_id, key)
+        parent_id = get_parent_folder(syn, project_id, key)
         file_id = syn.findEntityId(filename, parent_id)
         syn.delete(file_id)
 
-def get_parent_folder(project_id, key):
+def get_secret(secret_name, gc_project_name):
+    client = secretmanager.SecretManagerServiceClient()
+    resource_name = f"projects/{gc_project_name}/secrets/{secret_name}/versions/latest"
+    response = client.access_secret_version(resource_name)
+    return response.payload.data.decode('UTF-8')
+
+def get_parent_folder(syn, project_id, key):
     parent_id = project_id
     folders = key.split('/')
     folders=folders[:-1]
@@ -78,10 +93,7 @@ def get_parent_folder(project_id, key):
     for f in folders:
         folder_id = syn.findEntityId(f, parent_id)
         if folder_id == None:
-            # create folder
             folder_id = syn.store(synapseclient.Folder(name=f, parent=parent_id), forceVersion=False)['id']
         parent_id = folder_id
 
     return parent_id
-
-
